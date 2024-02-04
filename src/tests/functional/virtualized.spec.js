b2b519c1bc8b08162e212748ea9dbfda405c3b0d
Leigh Halliday <leighhalliday@gmail.com>
2024-02-04 10:57:29 -0500

    Adding additional tests

--- src/tests/functional/virtualized.spec.js
+++ src/tests/functional/virtualized.spec.js
@@ -11,10 +11,50 @@ test("removes row", async ({ page }) => {
   await page.goto("http://localhost:9000/items");
 
   await expect(page.locator("li").first().locator("span")).toHaveText("ID 1");
-  await page.locator("li").first().locator("button").click();
+  await page
+    .locator("li")
+    .first()
+    .locator("button", { hasText: "Delete" })
+    .click();
   await expect(page.locator("li").first().locator("span")).toHaveText("ID 2");
 });
 
-test("adds row via action", async ({ page }) => {
+test("updates row", async ({ page }) => {
   await page.goto("http://localhost:9000/items");
+
+  await expect(page.locator("li").first().locator("span")).toHaveText("ID 1");
+  await page
+    .locator("li")
+    .first()
+    .locator("button", { hasText: "Update" })
+    .click();
+  await expect(page.locator("li").first().locator("span")).toContainText(
+    "updated"
+  );
+});
+
+test("prepends row via action", async ({ page }) => {
+  await page.goto("http://localhost:9000/items");
+  await expect(page.locator("li").first().locator("span")).toHaveText("ID 1");
+
+  await page
+    .getByTestId("v-actions")
+    .locator("button", { hasText: "Prepend ID" })
+    .click();
+
+  const id = `ID ${Date.now()}`.substring(0, 10);
+  await expect(page.locator("li").first().locator("span")).toContainText(id);
+});
+
+test("prepends row via event", async ({ page }) => {
+  await page.goto("http://localhost:9000/items");
+  await expect(page.locator("li").first().locator("span")).toHaveText("ID 1");
+
+  await page
+    .getByTestId("v-actions")
+    .locator("button", { hasText: "Prepend Row" })
+    .click();
+
+  const id = `ID ${Date.now()}`.substring(0, 10);
+  await expect(page.locator("li").first().locator("span")).toContainText(id);
 });
